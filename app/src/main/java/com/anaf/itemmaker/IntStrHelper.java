c2429ca9161b7b81d8ff625c6bcdb703248601c6
Anaf Naufalian <kafri8889@gmail.com>
2020-07-12 20:52:24 +0700

    Added new Helper (AlertDialogMaker)

---
+++ app/src/main/java/com/anaf/itemmaker/IntStrHelper.java
@@ -0,0 +1,10 @@
+package com.anaf.itemmaker;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+public class IntStrHelper extends ContextWrapper {
+    public IntStrHelper(Context base) {
+        super(base);
+    }
+}
